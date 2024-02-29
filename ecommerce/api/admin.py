037ad1b8389f7a3d7f0024542700c3c58577707e
ChughtaiTalha <chughtai.talhahabib@gmail.com>
2024-03-01 00:10:09 +0500

    add models product, category and order

--- ecommerce/api/admin.py
+++ ecommerce/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product, Category, Order
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'category', 'create_at', 'updated_at')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'create_at', 'updated_at')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'category', 'quantity', 'price', 'status', 'create_at', 'updated_at')
--- ecommerce/api/models.py
+++ ecommerce/api/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=150, unique=True)
+
+
+class Order(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=[("Pending", "Pending"), ("Completed", "Completed")])
